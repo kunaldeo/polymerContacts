577cdf6bd7424a9a35a4d25994580dffbe62c64d
Kunal Deo <kunaldeo@gmail.com>
2015-02-26 00:48:39 +0530

    Map rename

--- main.dart
+++ main.dart
@@ -5,12 +5,12 @@ import 'src/template_data.dart';
 void main(List<String> args, SendPort port) {
   Receiver rec = new Receiver(port);
   rec.listen((Map<dynamic, dynamic> data) {
-    Map pluginData = new Map();
-    pluginData['info'] = "Polymer Contacts App";
-    pluginData['description'] = "A full fledged polymer app template with layout, route and ajax support";
-    pluginData['entrypoint'] = "web/index.html";
-    pluginData['help'] = "to run your app, use 'pub serve'";
-    pluginData['data'] = templateData;
-    rec.send(pluginData);
+    Map stagehandTemplateData = new Map();
+    stagehandTemplateData['info'] = "Polymer Contacts App";
+    stagehandTemplateData['description'] = "A full fledged polymer app template with layout, route and ajax support";
+    stagehandTemplateData['entrypoint'] = "web/index.html";
+    stagehandTemplateData['help'] = "to run your app, use 'pub serve'";
+    stagehandTemplateData['data'] = templateData;
+    rec.send(stagehandTemplateData);
   });
 }
